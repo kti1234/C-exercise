b911d30ce1971c9bf15cfefe70661e56fe4dccb8
kti1234 <113218217@qq.com>
2016-03-08 16:03:33 +0800

    Update sscanf_test.c

--- sscanf_test.c
+++ sscanf_test.c
@@ -6,8 +6,10 @@ int main(void){
     char b[5],c[5],d[5],e[5],f[5];
     int iRC;
 
+    /* sscanf 是一个很强大的字符串过滤函数，有很多高级用法，如果需要请自行google */
     iRC = sscanf(a,"%s %s i%s t%s %s", b, c, d, e, f);
-
+    
+    printf("iRC=%d",iRC);
     printf("%s\n",a);
     printf("%s\n",b);
     printf("%s\n",c);
